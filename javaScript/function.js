467d1a735f5a62867aa830e093b1469e89123708
Horaci Mota <horaciomota93@gmail.com>
2023-03-07 17:47:41 +0000

    Implemented basic functionality for todo app

--- javaScript/function.js
+++ javaScript/function.js
@@ -1,19 +1,25 @@
 let button = document.querySelector("button");
 button.addEventListener("click", function (event) {
+  console.log("clicou");
   event.preventDefault();
 
   //get value do input
 
-  let valueInput = document.querySelector(".inputAdd");
+  let valueInput = document.querySelector("form");
   let newTaskInput = valueInput.querySelector(".taskControl").value;
+
+  valueInput.reset();
 
   //Get the list
 
   let listOfElements = document.querySelector(".list-group");
   let newTaskAdded = document.createElement("li");
-  newTaskAdded.classList.add("list-group-item");
+  newTaskAdded.classList.add("teste");
   newTaskAdded.textContent = newTaskInput;
 
   // Put the new Task on the new list
   listOfElements.appendChild(newTaskAdded);
+
+  // TaskDone toggle
+  let doneOrNot = document.createElement("#");
 });
